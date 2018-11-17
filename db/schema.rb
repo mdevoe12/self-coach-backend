5595a504388ea17428bd47c234af03d2eed9769b
Matt DeVoe <matthew.devoe@gmail.com>
2018-11-17 16:37:49 -0700

    added items to user model with relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_005755) do
+ActiveRecord::Schema.define(version: 2018_11_17_233332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.float "data"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -25,4 +32,5 @@ ActiveRecord::Schema.define(version: 2018_11_05_005755) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
