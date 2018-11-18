79f41cfc8feb6cae6b63ff5eb21d9d789c62df3d
Matt DeVoe <matthew.devoe@gmail.com>
2018-11-17 19:37:40 -0700

    removed unneeded items

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:login]
 
-
   def login
-    pry
+    user = User.find_by(email: login_params[:email])
+
+    if user
+      render json: user.id
+    else
+      render json: {
+        status: 500,
+        message: 'Username or Password not found.'
+      }
+    end
+  end
+
+  private
+
+  def login_params
+    params.permit(:email, :password)
+  end
+
+  def hash_password(password)
+    BCrypt::Password.create(password).to_s
   end
 end
