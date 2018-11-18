e5b73f1fc12e14970a63153469d54e1f81b1a4a4
Matt DeVoe <matthew.devoe@gmail.com>
2018-11-17 17:10:33 -0700

    created initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Base seeds file for example data
+
+user = User.create(email: 'johndoe@sample.com', password: 'notpassword', firstname: 'John', lastname: 'Doe')
+
+main_lifts = {
+  'Squat' => 'BS',
+  'Deadlift' => 'DL',
+  'Press' => 'OHP',
+  'Bench' => 'BP'
+}
+# outer loop to create four main lifts
+main_lifts.each do |k, v|
+  item = user.items.create(name: k, abbreviation: v)
+  # inner loop creates fake data for each lift
+  data_number = 100
+  30.times do |n|
+    item.entries.create(date: (DateTime.now - (30 - n)), data: data_number)
+    data_number += 1
+  end
+end
